144fe9c0a7e015d4c8213199301d8d483821a00b
alexisbrabo <alexis_brabo@hotmail.com>
2018-07-05 16:41:12 -0300

    Primeiro Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
+
+import {TableModule} from 'primeng/table';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { VendasListagemComponent } from './vendas-listagem/vendas-listagem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    VendasListagemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    TableModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
